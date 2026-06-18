9fcbb21b834d96f522625d1798ab75d6494f5055
Ethan Tay <ethantay2003@gmail.com>
2026-06-18 21:59:36 +0800

    Add submit_limit for resting (non-crossing) orders

--- orderbook/book.py
+++ orderbook/book.py
@@ -22,3 +22,24 @@ class OrderBook:
             heapq.heappop(self._bid_heap)
         return -(self._bid_heap[0]) if self._bid_heap else None
     
+    def submit_limit(self, side, price, quantity):
+        #assigning the id and building the order
+        order_id = self._next_id
+        self._next_id += 1
+        order = Order(order_id = order_id, side = side, price = price, quantity = quantity, timestamp = order_id)
+
+        if side == "sell":
+            if price not in self._asks:
+                self._asks[price] = deque()    #creating a deque for the new price
+                heapq.heappush(self._ask_heap, price)    #pushing price to the heap
+            self._asks[price].append(order)    #append order to deque regardless of needing to create it or not
+        
+        elif side == "buy":
+            if price not in self._bids:
+                self._bids[price] = deque()
+                heapq.heappush(self._bid_heap, -price)
+            self._bids[price].append(order)
+
+        self._orders[order_id] = order
+        return []
+
