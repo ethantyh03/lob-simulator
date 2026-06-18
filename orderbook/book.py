5eda152488f4f22318f367a96fcd5532d713cb32
Ethan Tay <ethantay2003@gmail.com>
2026-06-18 18:05:08 +0800

    Add best_bid/best_ask with lazy deletion

--- orderbook/book.py
+++ orderbook/book.py
@@ -11,3 +11,14 @@ class OrderBook:
         self._bid_heap = []
         self._orders = {}
         self._next_id = 1
+
+    def best_ask(self):
+        while self._ask_heap and self._ask_heap[0] not in self._asks:
+            heapq.heappop(self._ask_heap)
+        return self._ask_heap[0] if self._ask_heap else None
+    
+    def best_bid(self):
+        while self._bid_heap and -(self._bid_heap[0]) not in self._bids:
+            heapq.heappop(self._bid_heap)
+        return -(self._bid_heap[0]) if self._bid_heap else None
+    
